b645575cc3775fdc5a3f94b966919dde82e1583f
miandaomian <miandaomian@qq.com>
2020-07-05 19:08:13 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    Home
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+<style lang="stylus">
+  .home
+    font-size 50px
+</style>
