f91205d4cd3379e08ae7858e1f609a0de9fc698e
miandaomian <miandaomian@qq.com>
2020-07-05 21:22:42 +0800

    2020.07.05 headerhome

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    Home
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
-
+  import HomeHeader from "./components/Header";
 export default {
   name: 'Home',
   components: {
+    HomeHeader
   }
 }
 </script>
 <style lang="stylus">
-  .home
-    font-size 50px
 </style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <i class="iconfont iconsousuo"></i>输入游玩城市
+        </div>
+        <div class="header-right">城市
+            <i class="iconfont iconjiantouxia"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeHeader',
+    }
+</script>
+<style lang="stylus" scoped>
+    @import "~@/assets/styles/varibles.styl"
+    .header
+        display flex
+        line-height .86rem
+        background $bgColor
+        color #fff
+        .header-left
+            width .64rem
+            float left
+        .back-icon
+            text-align center
+            font-size .4rem
+        .header-input
+            flex 1
+            height .64rem
+            line-height  .64rem
+            margin-top .12rem
+            margin-left .2rem
+            padding-left .2em
+            background #fff
+            border-radius .1rem
+            color #ccc
+        .header-right
+            width 1.24rem
+            float right
+            text-align center
+            .iconjiantouxia
+                margin-left -.04em
+                font-size .24em
+
+</style>
