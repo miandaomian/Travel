07fe2c2e74e140f750d0d20a364086fb8a5656bc
miandaomian <miandaomian@qq.com>
2020-07-09 11:22:50 +0800

    in finish

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,18 +1,22 @@
 <template>
     <div>
         <detail-banner></detail-banner>
+        <detial-header></detial-header>
+        <div class="content"></div>
     </div>
 
 </template>
 
 <script>
-    import DetailBanner from './components/Banner'
+    import DetailBanner from './components/Banner';
+    import DetialHeader from "./components/Header";
     export default {
         name: "Detail",
-        components: {DetailBanner}
+        components: {DetialHeader, DetailBanner}
     }
 </script>
 
 <style lang="stylus" scoped>
-
+    .content
+        height 50rem
 </style>
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="header-abs"></div>
+        <div class="hearder-fixed"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DetialHeader"
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .header-abs
+        position absolute
+        left .2rem
+        top .2rem
+        width .8rem
+        height .8rem
+        border-radius .4rem
+        line-height .8rem
+        text-align center
+        background rgba(0,0,0,.8)
+</style>
